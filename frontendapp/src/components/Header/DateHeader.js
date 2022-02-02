1079f5d0e75edeb454d8f4864b056964fd0bf98d
domino8788 <domino8788@naver.com>
2022-02-02 23:01:13 +0900

    [#2][수정] Header에 useStyles를 이용한 동적서식 적용

--- frontendapp/src/components/Header/DateHeader.js
+++ frontendapp/src/components/Header/DateHeader.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import moment from 'moment';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
+import { useStyles } from 'hooks';
 
 function DateHeader(props) {
-  const styles = makeStyles(props);
+  const styles = useStyles(makeStyles(props), [props.backgroundColor]);
   const formattedDate = moment(props.date).format('YYYY년 MM월 DD일');
   return (
     <View style={styles.block}>
@@ -17,16 +18,15 @@ DateHeader.defaultProps = {
   backgroundColor: '#26a69a',
 };
 
-const makeStyles = (props) =>
-  StyleSheet.create({
-    block: {
-      padding: 16,
-      backgroundColor: props.backgroundColor,
-    },
-    dateText: {
-      fontSize: 24,
-      color: 'white',
-    },
-  });
+const makeStyles = (props) => ({
+  block: {
+    padding: 16,
+    backgroundColor: props.backgroundColor,
+  },
+  dateText: {
+    fontSize: 24,
+    color: 'white',
+  },
+});
 
 export default DateHeader;
