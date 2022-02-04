2245d33102b69033b374034e7d55c38a2200d156
domino8788 <domino8788@naver.com>
2022-02-04 23:49:16 +0900

    [#2][수정] Todo 페이지 리스트 구현

--- frontendapp/src/components/List/index.js
+++ frontendapp/src/components/List/index.js
@@ -1,4 +1,30 @@
+import React from 'react';
+import { FlatList, View, StyleSheet } from 'react-native';
+
+import Item from './Item';
 import Empty from './Empty';
 import AddItem from './AddItem';
 
-export default { Empty, AddItem };
+const Linear = (props) => {
+  return (
+    <FlatList
+      ItemSeparatorComponent={() => <View style={styles.separator} />}
+      style={styles.list}
+      data={props.data}
+      renderItem={({ item }) => <Item id={item.id} text={item.text} done={item.done} />}
+      keyExtractor={(item) => item.id.toString()}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+  },
+  separator: {
+    backgroundColor: '#e0e0e0',
+    height: 1,
+  },
+});
+
+export default { Linear, Empty, Item, AddItem };
--- frontendapp/src/screens/Todo/index.js
+++ frontendapp/src/screens/Todo/index.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, StatusBar } from 'react-native';
 import { Header, List } from 'components';
 import image_young_and_happy from 'assets/images/young_and_happy.png';
 
 const defaultColor = '#26a69a';
 
-function Todo() {
+const Todo = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: '작업환경 설정', done: true },
+    { id: 2, text: '리액트 네이티브 기초 공부', done: false },
+    { id: 3, text: '투두리스트 만들어보기', done: false },
+  ]);
   return (
     <>
       <StatusBar backgroundColor={defaultColor} barStyle="light-content" />
       <Header.DateHeader backgroundColor={defaultColor} />
-      <List.Empty image={image_young_and_happy} text="야호! 할일이 없습니다." />
+      {todos.length === 0 ? <List.Empty image={image_young_and_happy} text="야호! 할일이 없습니다." /> : <List.Linear data={todos} />}
       <List.AddItem placeholder="할일을 입력하세요." />
     </>
   );
-}
+};
 
 const styles = StyleSheet.create({});
 
