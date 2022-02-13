a0a2678a8b03b30ec9cac3e59b5f54621eb52589
domino8788 <domino8788@naver.com>
2022-02-13 23:31:44 +0900

    [#2][추가] navigator 적용

--- frontendapp/src/App.js
+++ frontendapp/src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Todo } from 'screens';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.block} edges={['bottom']}>
-        <KeyboardAvoidingView behavior={Platform.select({ ios: 'padding' })} style={styles.avoid}>
-          <Todo />
-        </KeyboardAvoidingView>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <SafeAreaView style={styles.block} edges={['bottom']}>
+          <KeyboardAvoidingView behavior={Platform.select({ ios: 'padding' })} style={styles.avoid}>
+            <Stack.Navigator initialRouteName="Todo">
+              <Stack.Screen name="Todo" component={Todo} />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </NavigationContainer>
   );
 };
 const styles = StyleSheet.create({
