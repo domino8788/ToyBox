c8da63343b769d00b2d1e3db069b3fb844ccda94
domino8788 <domino8788@naver.com>
2022-02-16 23:22:15 +0900

    [#2][수정] reanimated 라이브러리 관련 이슈 수정

--- frontendapp/src/App.js
+++ frontendapp/src/App.js
@@ -3,24 +3,26 @@ import { StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Home, Todo } from 'screens';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <SafeAreaProvider>
-        <SafeAreaView style={styles.block} edges={['bottom']}>
-          <KeyboardAvoidingView behavior={Platform.select({ ios: 'padding' })} style={styles.avoid}>
-            <Stack.Navigator initialRouteName="Home">
-              <Stack.Screen name="Home" component={Home} />
-              <Stack.Screen name="Todo" component={Todo} />
-            </Stack.Navigator>
-          </KeyboardAvoidingView>
-        </SafeAreaView>
-      </SafeAreaProvider>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.block} edges={['bottom']}>
+        <KeyboardAvoidingView behavior={Platform.select({ ios: 'padding' })} style={styles.avoid}>
+          <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Home" drawerPosition="left" backBehavior="history">
+              <Drawer.Screen name="Home" component={Home} />
+              <Drawer.Screen name="Todo" component={Todo} />
+            </Drawer.Navigator>
+          </NavigationContainer>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 const styles = StyleSheet.create({
