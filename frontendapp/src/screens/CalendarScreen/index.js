97eb433bbff78b9024fcc2f103994e8ba657da6c
domino8788 <domino8788@naver.com>
2022-02-24 23:31:59 +0900

    [#3][수정] DayLog의 각 페이지에 FloatingButton 추가

--- frontendapp/src/screens/CalendarScreen/index.js
+++ frontendapp/src/screens/CalendarScreen/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { FloatingButton } from 'components';
 
 function CalendarScreen() {
-  return <View style={styles.block} />;
+  return (
+    <View style={styles.block}>
+      <FloatingButton />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  block: {},
+  block: { flex: 1 },
 });
 
 export default CalendarScreen;
--- frontendapp/src/screens/FeedsScreen/index.js
+++ frontendapp/src/screens/FeedsScreen/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { FloatingButton } from 'components';
 
 const FeedsScreen = () => {
-  return <View style={styles.block} />;
+  return (
+    <View style={styles.block}>
+      <FloatingButton />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-  block: {},
+  block: { flex: 1 },
 });
 
 export default FeedsScreen;
