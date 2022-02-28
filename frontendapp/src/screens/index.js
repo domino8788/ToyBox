07d67598b8984e22e42a563aae0406473fcb22ff
domino8788 <domino8788@naver.com>
2022-02-28 23:09:27 +0900

    [#3][수정] 오타 수정

--- frontendapp/src/screens/index.js
+++ frontendapp/src/screens/index.js
@@ -3,7 +3,7 @@ import TodoScreen from 'screens/TodoScreen';
 import CalendarScreen from 'screens/CalendarScreen';
 import FeedsScreen from 'screens/FeedsScreen';
 import SearchScreen from 'screens/SearchScreen';
-import WriteScreen from 'screens/SearchScreen';
+import WriteScreen from 'screens/WriteScreen';
 import DayLogScreen from 'screens/DayLogScreen';
 
 export { HomeScreen, TodoScreen, CalendarScreen, FeedsScreen, SearchScreen, WriteScreen, DayLogScreen };
