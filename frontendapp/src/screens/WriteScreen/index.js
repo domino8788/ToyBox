8eed8de732ab8aa3332edfef14caefc99dd33c27
domino8788 <domino8788@naver.com>
2022-03-01 23:46:26 +0900

    [#3][수정] WriteScreen 초안 구현

--- frontendapp/src/screens/WriteScreen/index.js
+++ frontendapp/src/screens/WriteScreen/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Platform, KeyboardAvoidingView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Input, Header } from 'components';
 
 const WriteScreen = () => {
-  return <View style={styles.block} />;
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  return (
+    <SafeAreaView style={styles.block}>
+      <KeyboardAvoidingView style={styles.avoidingView} behavior={Platform.select({ ios: 'padding' })}>
+        <Header.Write />
+        <Input.Editor
+          title={title}
+          titlePlaceholder="제목을 입력하세요"
+          body={body}
+          bodyPlaceholder="당신의 오늘을 기록해보세요"
+          onChangeTitle={setTitle}
+          onChangeBody={setBody}
+        />
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-  block: {},
+  block: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  avoidingView: {
+    flex: 1,
+  },
 });
 
 export default WriteScreen;
