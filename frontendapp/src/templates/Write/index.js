12967a4cf4d0c71114dc380cb0571540181faae8
domino8788 <domino8788@naver.com>
2022-03-21 22:38:14 +0900

    [#3][수정] DayLog의 Write 페이지에서 Header 중앙에 날짜 정보를 표시하게 수정

--- frontendapp/src/templates/Write/index.js
+++ frontendapp/src/templates/Write/index.js
@@ -1,14 +1,32 @@
-import React, { useState, useContext, useCallback } from 'react';
-import { StyleSheet, Platform, KeyboardAvoidingView, Alert } from 'react-native';
+import React, { useState, useContext, useCallback, useMemo } from 'react';
+import { StyleSheet, Platform, KeyboardAvoidingView, Alert, Pressable, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 import { Input, Header } from 'components';
+import { formatDate } from 'helpers';
 import Context, { INSERT_LOG, UPDATE_LOG, DELETE_LOG } from 'stores/Context';
+
+const CenterDateText = ({ date }) => {
+  const dateText = useMemo(() => formatDate(new Date(date), 'PPP'), [date]);
+  const timeText = useMemo(() => formatDate(new Date(date), 'p'), [date]);
+  return (
+    <>
+      <Pressable>
+        <Text style={styles.text}>{dateText}</Text>
+      </Pressable>
+      <View style={styles.separator} />
+      <Pressable>
+        <Text style={styles.text}>{timeText}</Text>
+      </Pressable>
+    </>
+  );
+};
 
 const Write = ({ route }) => {
   const log = route.params?.log;
   const [title, setTitle] = useState(log?.title ?? '');
   const [body, setBody] = useState(log?.body ?? '');
+  const [date, setDate] = useState(log ? new Date(log.date) : new Date());
   const [, dispatch] = useContext(Context);
   const navigation = useNavigation();
   const onSave = useCallback(() => {
@@ -54,7 +72,13 @@ const Write = ({ route }) => {
   return (
     <SafeAreaView style={styles.block}>
       <KeyboardAvoidingView style={styles.avoidingView} behavior={Platform.select({ ios: 'padding' })}>
-        <Header.Write onSave={onSave} onDelete={onDelete} isEditing={!!log} />
+        <Header.Write
+          onSave={onSave}
+          onDelete={onDelete}
+          isEditing={!!log}
+          center={log ? <CenterDateText date={date} /> : ''}
+          onChangeDate={setDate}
+        />
         <Input.Editor
           title={title}
           titlePlaceholder="제목을 입력하세요"
@@ -75,6 +99,12 @@ const styles = StyleSheet.create({
   },
   avoidingView: {
     flex: 1,
+  },
+  separator: {
+    width: 8,
+  },
+  text: {
+    color: '#212121',
   },
 });
 
