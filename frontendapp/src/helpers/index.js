980a8da4565df44018930d5f0a7226a87bff2001
domino8788 <domino8788@naver.com>
2022-03-22 23:00:55 +0900

    [#3][수정] formatDate helper 함수가 시간차 표시 포맷을 옵션에 따라 표시하게 수정

--- frontendapp/src/helpers/index.js
+++ frontendapp/src/helpers/index.js
@@ -1,16 +1,16 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import { ko } from 'date-fns/locale';
 
-export const formatDate = (date, formatString = 'yyyy-MM-dd', option = { locale: ko }) => {
+export const formatDate = (date, formatString = 'yyyy-MM-dd', formatOption = { useDistance: false }, option = { locale: ko }) => {
   const d = new Date(date);
   const now = Date.now();
   const diff = (now - d.getTime()) / 1000;
-
-  if (diff < 60 * 1) {
-    return '방금 전';
-  }
-  if (diff < 60 * 60 * 24 * 3) {
-    return formatDistanceToNow(d, { addSuffix: true, ...option });
+  if (formatOption?.useDistance) {
+    if (diff < 60 * 1) {
+      return '방금 전';
+    } else if (diff < 60 * 60 * 24 * 3) {
+      return formatDistanceToNow(d, { addSuffix: true, ...option });
+    }
   }
   return format(d, formatString, option);
 };
