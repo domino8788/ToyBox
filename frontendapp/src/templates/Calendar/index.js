ed59b2c6c9fc57ae64ebb3f6268c06c4f9ddd70d
domino8788 <domino8788@naver.com>
2022-03-22 23:02:49 +0900

    [#3][수정] DayLog의 Calendar 페이지에서 리스트 클릭 시 수정 페이지를 표시하게 수정

--- frontendapp/src/templates/Calendar/index.js
+++ frontendapp/src/templates/Calendar/index.js
@@ -1,11 +1,13 @@
-import React, { useState, useContext, useMemo } from 'react';
+import React, { useState, useContext, useMemo, useCallback } from 'react';
 import { CustomView, List } from 'components';
 import Context from 'stores/Context';
 import { formatDate } from 'helpers';
+import { useNavigation } from '@react-navigation/native';
 
 const Calendar = () => {
   const [{ logs }] = useContext(Context);
   const [selectedDate, setSelectedDate] = useState(formatDate(new Date(), 'yyyy-MM-dd'));
+  const navigation = useNavigation();
   const markedDates = useMemo(
     () =>
       logs.reduce((acc, current) => {
@@ -15,6 +17,12 @@ const Calendar = () => {
       }, {}),
     [logs],
   );
+  const onItemPress = useCallback(
+    (log) => {
+      navigation.navigate('Write', { log });
+    },
+    [navigation],
+  );
   const filteredLogs = useMemo(() => logs.filter((log) => formatDate(new Date(log.date), 'yyyy-MM-dd') === selectedDate), [logs, selectedDate]);
 
   return (
@@ -22,6 +30,7 @@ const Calendar = () => {
       data={filteredLogs}
       item={List.Item.Feed}
       header={<CustomView.Calendar markedDates={markedDates} selectedDate={selectedDate} onSelectDate={setSelectedDate} />}
+      onPress={onItemPress}
     />
   );
 };
