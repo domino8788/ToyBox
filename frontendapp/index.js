c10ae39201b1c64f50ee09e73b86388805219c89
domino8788 <domino8788@naver.com>
2022-03-31 23:39:31 +0900

    [#1][수정] react-native-config 를 이용한 빌드 설정 적용

--- frontendapp/index.js
+++ frontendapp/index.js
@@ -3,5 +3,17 @@ import App from './src/App';
 import { name as appName } from './app.json';
 import 'react-native-get-random-values';
 import storybook from './storybook';
+import Config from 'react-native-config';
 
-AppRegistry.registerComponent(appName, () => storybook);
+let Root = App;
+switch (Config.BUILD_ENV) {
+  case 'STORYBOOK': {
+    Root = storybook;
+    break;
+  }
+  default: {
+    Root = App;
+  }
+}
+
+AppRegistry.registerComponent(appName, () => Root);
