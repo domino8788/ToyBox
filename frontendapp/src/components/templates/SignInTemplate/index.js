5636fe88393e4351bb185a59f60d5ac52beae243
domino8788 <domino8788@naver.com>
2022-04-06 22:12:26 +0900

    [#4][수정] 로그인, 회원가입 실패에 따른 안내문구 추가

--- frontendapp/src/components/templates/SignInTemplate/index.js
+++ frontendapp/src/components/templates/SignInTemplate/index.js
@@ -27,15 +27,32 @@ const SignInTemplate = ({ route }) => {
   );
   const onSubmit = async () => {
     Keyboard.dismiss();
-    const { email, password } = form;
+    const { email, password, confirmPassword } = form;
+
+    if (isSignUp && password !== confirmPassword) {
+      Alert.alert('실패', '비밀번호가 일치하지 않습니다.');
+      return;
+    }
+
+    if (!email.trim().length || !password.trim().length) {
+      Alert.alert('실패', '이메일과 비밀번호를 입력해주세요.');
+      return;
+    }
+
     const info = { email, password };
     handleIsLoading(true);
     try {
       const { user } = isSignUp ? await signUp(info) : await signIn(info);
       console.log(user);
     } catch (e) {
-      Alert.alert('실패');
-      console.log(e);
+      const messages = {
+        'auth/email-already-in-use': '이미 가입된 이메일입니다.',
+        'auth/wrong-password': '잘못된 비밀번호입니다.',
+        'auth/user-not-found': '존재하지 않는 계정입니다.',
+        'auth/invalid-email': '유효하지 않은 이메일 주소입니다.',
+      };
+      const msg = messages[e.code] || `${isSignUp ? '가입' : '로그인'} 실패`;
+      Alert.alert('실패', msg);
     } finally {
       handleIsLoading(false);
     }
